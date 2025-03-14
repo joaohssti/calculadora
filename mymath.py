de35ffa382aa9f5f6455fa8ec77e9b9c9bb57886
João Silva <joaohssti@gmail.com>
2025-03-14 14:01:30 -0300

    funcoes matematicas basicas

---
+++ mymath.py
@@ -0,0 +1,45 @@
+
+
+def somar(parcela_1, parcela_2) -> float:
+    return parcela_1 + parcela_2
+
+
+def subtrair(aditivo_1, subtrativo_2) -> float:
+    return aditivo_1 - subtrativo_2
+
+
+def multiplicar(fator_1, fator_2) -> float:
+    return fator_1 * fator_2
+
+
+def dividir(dividendo, divisor, option:bool=True) -> float:
+    if divisor == 0:
+        return "Entrada inválida"
+    if option:
+        return dividendo / divisor
+    quociente_inteiro = dividendo // divisor
+    resto = dividendo % divisor
+    return [quociente_inteiro, resto]
+
+
+def potenciar(num, expoente) -> float:
+    return num ** expoente
+
+
+def raiz_quadrada(radicando):
+    return
+
+
+def fatorial(num:int) -> int:
+    if num < 0:
+        return "Entrada inválida"
+    if num <= 1:
+        return 1
+    contagem = 2
+    soma = num
+    while contagem != num:
+        soma *= contagem
+        contagem += 1
+    return soma
+
+
